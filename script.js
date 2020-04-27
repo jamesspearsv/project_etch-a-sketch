71e000cdcbd7259742d7745c47a58230abc2de7b
James Spears V <jamesspearsV@gmail.com>
2020-04-26 23:47:11 -0400

    Added functioning hover effect to grid

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-//Loads canvas grid on page load
-const canvas = document.querySelector('.canvas');
-window.onload = () => {
-    var i;
-    for (i=0; i < 256; i++) {
-      const div = document.createElement('div');
-      div.classList.add('square');
-      canvas.appendChild(div);
-    };
+const grid = document.querySelector('.grid');
+
+//Loads grid on page load
+function createGrid () {
+  var i;
+  for (i=0; i < 256; i++) {
+  const div = document.createElement('div');
+  div.classList.add('gridItem');
+  grid.appendChild(div);
+  };
 };
+createGrid();
+
+//Hover effect to change background color
+const gridItem = document.querySelectorAll ('.gridItem');
+gridItem.forEach((gridItem) => {
+  gridItem.addEventListener('mouseenter', (e) => {
+    e.target.style.backgroundColor = 'black';
+  });
+});
